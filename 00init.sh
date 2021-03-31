c7da55d09d73485bce15aa896d7820112108e3af
Kipp Cannon <kipp.cannon@ligo.org>
2021-03-31 15:53:32 +0900

    00boot.sh --> 00init.sh + a few tweaks

--- 00init.sh
+++ 00init.sh
@@ -7,11 +7,9 @@ read -p "Press CTRL-C to abort, or RETURN to continue... " INPUT
 # Get it done
 {
 	{ echo "running aclocal (please ignore \"underquoted\" warnings)..." ; aclocal -I gnuscripts ; } &&
+	{ echo "running libtoolize..." ; libtoolize -c -f || glibtoolize $LIBTOOLIZE_FLAGS ; } &&
 	{ echo "running autoheader..." ; autoheader ; } &&
 	{ echo "running automake..." ; automake -a -c ; } &&
 	{ echo "running autoconf..." ; autoconf ; } &&
 	echo "$0 complete." ;
 } || { echo "$0 failed." ; false ; }
-
-# FIXME
-ln -s ../../ezxml/ezxml.h src/include/libezligolw
