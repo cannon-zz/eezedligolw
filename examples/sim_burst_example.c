db1bef8e36d2511712d659cebcec34e5b0a36bd5
Kipp Cannon <kipp.cannon@ligo.org>
2021-03-31 15:16:04 +0900

    snapshot circa 2009
    this package used to live in the "lsware" CVS project hosted on gravity.phys.uwm.edu.  that server was taken down years ago.  this snapshot has been recovered from a working copy I had in my home directory.
    
    I started this project in about 2007 as a proof-of-principle to demonstrate how straight-forward it would be to separate lalsuite from libmetaio, in the hope that people would see the benefit.  the idea didn't get enough up-take:  I wasn't prepared to fix other people's codes for them, they were going to have to want to improve their tools for their own reasons, and people didn't.  years later when the gstlal project needed to move forward with XML format changes, specifically changing the "ilwd:char" row IDs to integers, and the rest of the collaboration wouldn't agree to it, this code was resurrected and added to the gstlal project allowing gstlal to decouple from lalsuite and make XML table format changes independently of the collaboration.
    
    more recently, a studnent of mine has found it necessary to read PSD arrays in C code, and, of course, that's impossible because why would anybody provide an I/O library for a file format they invented, I mean, sheesh, what do you think this is?  we could write a python translator to convert the PSDs from LIGO LW XML to something that can be read in C, like HDF5, but then an HDF5 PSD extractor would still need to be written, plus using any resulting tool would require an external translation step to be run on the PSDs first which is inconvenient.  it's probably the same total lines of code to just add the capability directly in C here, and the result will be more convenient to use.  so that's what I'm doing.

---
+++ examples/sim_burst_example.c
@@ -0,0 +1,179 @@
+/*
+ * $Id: sim_burst_example.c,v 1.2 2007/06/23 22:49:03 kipp Exp $
+ *
+ * Copyright (C) 2007  Kipp Cannon
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation; either version 2 of the License, or (at your
+ * option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General
+ * Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+
+#include <libezligolw/ezligolw.h>
+#include <lal/LIGOLwXML.h>
+#include <lal/LIGOMetadataTables.h>
+#include <lal/LIGOMetadataUtils.h>
+
+
+/*
+ * Demonstration of the implementation of an XLAL-style drop-in replacement
+ * of LALSimBurstTableFromLIGOLw() built on top of ezligolw.  For clarity,
+ * the LAL code's ability to filter the input trigger list by GPS time has
+ * not been implemented here.
+ */
+
+
+static int sim_burst_row_callback(struct ligolw_table *table, struct ligolw_table_row row, void *data)
+{
+	int result_code;
+	SimBurst **head = data;
+	SimBurst *new = XLALCreateSimBurst();
+	struct ligolw_unpacking_spec sim_burst_spec[] = {
+		{NULL, "process_id", ligolw_cell_type_ilwdchar, LIGOLW_UNPACKING_REQUIRED},
+		{NULL, "waveform", ligolw_cell_type_lstring, LIGOLW_UNPACKING_REQUIRED},
+		{&new->ra, "ra", ligolw_cell_type_real_8, LIGOLW_UNPACKING_REQUIRED},
+		{&new->dec, "dec", ligolw_cell_type_real_8, LIGOLW_UNPACKING_REQUIRED},
+		{&new->psi, "psi", ligolw_cell_type_real_8, LIGOLW_UNPACKING_REQUIRED},
+		{&new->time_geocent_gps.gpsSeconds, "time_geocent_gps", ligolw_cell_type_int_4s, LIGOLW_UNPACKING_REQUIRED},
+		{&new->time_geocent_gps.gpsNanoSeconds, "time_geocent_gps_ns", ligolw_cell_type_int_4s, LIGOLW_UNPACKING_REQUIRED},
+		{&new->time_geocent_gmst, "time_geocent_gmst", ligolw_cell_type_real_8, LIGOLW_UNPACKING_REQUIRED},
+		{&new->duration, "duration", ligolw_cell_type_real_8, LIGOLW_UNPACKING_REQUIRED},
+		{&new->frequency, "frequency", ligolw_cell_type_real_8, LIGOLW_UNPACKING_REQUIRED},
+		{&new->bandwidth, "bandwidth", ligolw_cell_type_real_8, LIGOLW_UNPACKING_REQUIRED},
+		{&new->q, "q", ligolw_cell_type_real_8, LIGOLW_UNPACKING_REQUIRED},
+		{&new->pol_ellipse_angle, "pol_ellipse_angle", ligolw_cell_type_real_8, LIGOLW_UNPACKING_REQUIRED},
+		{&new->pol_ellipse_e, "pol_ellipse_e", ligolw_cell_type_real_8, LIGOLW_UNPACKING_REQUIRED},
+		{&new->amplitude, "amplitude", ligolw_cell_type_real_8, LIGOLW_UNPACKING_REQUIRED},
+		{&new->hrss, "hrss", ligolw_cell_type_real_8, LIGOLW_UNPACKING_REQUIRED},
+		{&new->egw_over_rsquared, "egw_over_rsquared", ligolw_cell_type_real_8, LIGOLW_UNPACKING_REQUIRED},
+		{&new->waveform_number, "waveform_number", ligolw_cell_type_int_8u, LIGOLW_UNPACKING_REQUIRED},
+		{NULL, "simulation_id", ligolw_cell_type_ilwdchar, LIGOLW_UNPACKING_REQUIRED},
+		{NULL, NULL, -1, 0}
+	};
+
+	/* check for memory allocation failure.  remember to clean up row's
+	 * memory. */
+	if(!new) {
+		XLALPrintError("memory allocation failure\n");
+		free(row.cells);
+		return -1;
+	}
+
+	/* unpack.  have to do the strings manually because they get copied
+	 * by value rather than reference.  ligolw_unpacking_row_builder()
+	 * cleans up row's memory for us. */
+	strncpy(new->waveform, ligolw_row_get_cell(row, "waveform").as_string, LIGOMETA_WAVEFORM_MAX - 1);
+	new->waveform[LIGOMETA_WAVEFORM_MAX] = '\0';
+
+	result_code = ligolw_unpacking_row_builder(table, row, sim_burst_spec);
+	if(result_code > 0) {
+		/* missing required column */
+		XLALPrintError("failure parsing row: missing column \"%s\"\n", sim_burst_spec[result_code - 1].name);
+		free(new);
+		return -1;
+	} else if(result_code < 0) {
+		/* column type mismatch */
+		XLALPrintError("failure parsing row: incorrect type for column \"%s\"\n", sim_burst_spec[-result_code - 1].name);
+		free(new);
+		return -1;
+	}
+
+	/* add new sim to head of linked list.  yes, this means the table's
+	 * rows get reversed.  so what. */
+	new->next = *head;
+	*head = new;
+
+	/* success */
+	return 0;
+}
+
+
+int XLALSimBurstTableFromLIGOLw(
+	SimBurst **head,
+	const char *filename,
+	int gps_start,
+	int gps_end
+)
+{
+	static const char func[] = "XLALSimBurstTableFromLIGOLw";
+	ezxml_t xmldoc;
+	ezxml_t elem;
+	struct ligolw_table *table;
+
+	/* initialize the linked list */
+	*head = NULL;
+
+	/* parse the document */
+	xmldoc = ezxml_parse_file(filename);
+	if(!xmldoc) {
+		XLALPrintError("error parsing %s\n", filename);
+		XLAL_ERROR(func, XLAL_EIO);
+	}
+
+	/* find the sim_burst table */
+	elem = ligolw_table_get(xmldoc, "sim_burst");
+	if(!elem) {
+		XLALPrintError("unable to locate sim_burst table\n");
+		ezxml_free(xmldoc);
+		XLAL_ERROR(func, XLAL_EDATA);
+	}
+
+	/* convert the rows into a LAL-style linked list */
+	table = ligolw_table_parse(elem, sim_burst_row_callback, head);
+	if(!table) {
+		XLALPrintError("failure parsing sim_burst table\n");
+		ezxml_free(xmldoc);
+		XLAL_ERROR(func, XLAL_EDATA);
+	}
+
+	/* clean up */
+	ligolw_table_free(table);
+	ezxml_free(xmldoc);
+
+	/* success */
+	return 0;
+}
+
+
+/*
+ * Example use.
+ */
+
+
+static void write(const SimBurst *sims, const char *filename)
+{
+	LALStatus status;
+	LIGOLwXMLStream xmlfp;
+
+	memset(&status, 0, sizeof(status));
+	memset(&xmlfp, 0, sizeof(xmlfp));
+
+	LALOpenLIGOLwXMLFile(&status, &xmlfp, filename);
+	XLALWriteLIGOLwXMLSimBurstTable(&xmlfp, sims);
+	LALCloseLIGOLwXMLFile(&status, &xmlfp);
+}
+
+
+
+int main(int argc, char *argv[])
+{
+	SimBurst *sims;
+
+	lalDebugLevel = LALERROR | LALNMEMDBG | LALNMEMPAD | LALNMEMTRK;
+
+	XLALSimBurstTableFromLIGOLw(&sims, "HL-INJECTIONS_PLAYGROUND-793154935-2524278.xml", 0, 0);
+
+	write(sims, "output.xml");
+
+	return 0;
+}
