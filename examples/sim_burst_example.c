202399fe66d5aec0e75b126ad63579514cc1cf81
Kipp Cannon <kipp@g.ecc.u-tokyo.ac.jp>
2026-03-17 14:10:16 +0900

    sim_burst_example:
    - move sim_burst_row_callback() to separate file.

--- examples/sim_burst_example.c
+++ examples/sim_burst_example.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2007  Kipp Cannon
+ * Copyright (C) 2007,2026  Kipp Cannon
  *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms of the GNU General Public License as published by the
@@ -24,138 +24,15 @@
 #include <ezligolw/ezligolw.h>
 
 
+int sim_burst_row_callback(struct ligolw_table *, struct ligolw_table_row *, void *);
+
+
 /*
  * Demonstration of the implementation of an XLAL-style (near) drop-in
  * replacement of LALSimBurstTableFromLIGOLw() built on top of ezligolw.
  * For clarity, the LAL code's ability to filter the input trigger list by
  * GPS time has not been implemented here.
  */
-
-
-static int sim_burst_row_callback(struct ligolw_table *table, struct ligolw_table_row *row, void *data)
-{
-	int result_code;
-	SimBurst **head = data;
-	SimBurst *new = XLALCreateSimBurst();
-	union ligolw_cell *str;
-	struct ligolw_unpacking_spec *spec;
-	struct ligolw_unpacking_spec sim_burst_basic[] = {
-		{"process:process_id", &new->process_id, NULL, ligolw_cell_type_int_8s, LIGOLW_COLUMN_FLAGS_REQUIRED},
-		{"waveform", NULL, NULL, ligolw_cell_type_lstring, LIGOLW_COLUMN_FLAGS_REQUIRED},
-		{"ra", &new->ra, NULL, ligolw_cell_type_real_8, LIGOLW_COLUMN_FLAGS_REQUIRED},
-		{"dec", &new->dec, NULL, ligolw_cell_type_real_8, LIGOLW_COLUMN_FLAGS_REQUIRED},
-		{"psi", &new->psi, NULL, ligolw_cell_type_real_8, LIGOLW_COLUMN_FLAGS_REQUIRED},
-		{"time_geocent_gps", &new->time_geocent_gps.gpsSeconds, NULL, ligolw_cell_type_int_4s, LIGOLW_COLUMN_FLAGS_REQUIRED},
-		{"time_geocent_gps_ns", &new->time_geocent_gps.gpsNanoSeconds, NULL, ligolw_cell_type_int_4s, LIGOLW_COLUMN_FLAGS_REQUIRED},
-		{"time_geocent_gmst", &new->time_geocent_gmst, NULL, ligolw_cell_type_real_8, LIGOLW_COLUMN_FLAGS_REQUIRED},
-		{"time_slide:time_slide_id", &new->time_slide_id, NULL, ligolw_cell_type_int_8s, LIGOLW_COLUMN_FLAGS_REQUIRED},
-		{"simulation_id", &new->simulation_id, NULL, ligolw_cell_type_int_8s, LIGOLW_COLUMN_FLAGS_REQUIRED},
-		{NULL, NULL, NULL, -1, 0}
-	};
-	struct ligolw_unpacking_spec sim_burst_StringCusp[] = {
-		{"duration", &new->duration, NULL, ligolw_cell_type_real_8, LIGOLW_COLUMN_FLAGS_REQUIRED},
-		{"frequency", &new->frequency, NULL, ligolw_cell_type_real_8, LIGOLW_COLUMN_FLAGS_REQUIRED},
-		{"amplitude", &new->amplitude, NULL, ligolw_cell_type_real_8, LIGOLW_COLUMN_FLAGS_REQUIRED},
-		{NULL, NULL, NULL, -1, 0}
-	};
-	struct ligolw_unpacking_spec sim_burst_SineGaussian[] = {
-		{"duration", &new->duration, NULL, ligolw_cell_type_real_8, LIGOLW_COLUMN_FLAGS_REQUIRED},
-		{"frequency", &new->frequency, NULL, ligolw_cell_type_real_8, LIGOLW_COLUMN_FLAGS_REQUIRED},
-		{"bandwidth", &new->bandwidth, NULL, ligolw_cell_type_real_8, LIGOLW_COLUMN_FLAGS_REQUIRED},
-		{"q", &new->q, NULL, ligolw_cell_type_real_8, LIGOLW_COLUMN_FLAGS_REQUIRED},
-		{"pol_ellipse_angle", &new->pol_ellipse_angle, NULL, ligolw_cell_type_real_8, LIGOLW_COLUMN_FLAGS_REQUIRED},
-		{"pol_ellipse_e", &new->pol_ellipse_e, NULL, ligolw_cell_type_real_8, LIGOLW_COLUMN_FLAGS_REQUIRED},
-		{"hrss", &new->hrss, NULL, ligolw_cell_type_real_8, LIGOLW_COLUMN_FLAGS_REQUIRED},
-		{NULL, NULL, NULL, -1, 0}
-	};
-	struct ligolw_unpacking_spec sim_burst_Gaussian[] = {
-		{"duration", &new->duration, NULL, ligolw_cell_type_real_8, LIGOLW_COLUMN_FLAGS_REQUIRED},
-		{"hrss", &new->hrss, NULL, ligolw_cell_type_real_8, LIGOLW_COLUMN_FLAGS_REQUIRED},
-		{NULL, NULL, NULL, -1, 0}
-	};
-	struct ligolw_unpacking_spec sim_burst_BTLWNB[] = {
-		{"duration", &new->duration, NULL, ligolw_cell_type_real_8, LIGOLW_COLUMN_FLAGS_REQUIRED},
-		{"frequency", &new->frequency, NULL, ligolw_cell_type_real_8, LIGOLW_COLUMN_FLAGS_REQUIRED},
-		{"bandwidth", &new->bandwidth, NULL, ligolw_cell_type_real_8, LIGOLW_COLUMN_FLAGS_REQUIRED},
-		{"pol_ellipse_angle", &new->pol_ellipse_angle, NULL, ligolw_cell_type_real_8, LIGOLW_COLUMN_FLAGS_REQUIRED},
-		{"pol_ellipse_e", &new->pol_ellipse_e, NULL, ligolw_cell_type_real_8, LIGOLW_COLUMN_FLAGS_REQUIRED},
-		{"egw_over_rsquared", &new->egw_over_rsquared, NULL, ligolw_cell_type_real_8, LIGOLW_COLUMN_FLAGS_REQUIRED},
-		{"waveform_number", &new->waveform_number, NULL, ligolw_cell_type_int_8u, LIGOLW_COLUMN_FLAGS_REQUIRED},
-		{NULL, NULL, NULL, -1, 0}
-	};
-	struct ligolw_unpacking_spec sim_burst_Impulse[] = {
-		{"amplitude", &new->amplitude, NULL, ligolw_cell_type_real_8, LIGOLW_COLUMN_FLAGS_REQUIRED},
-		{NULL, NULL, NULL, -1, 0}
-	};
-
-	/* check for memory allocation failure.  remember to clean up row's
-	 * memory. */
-	if(!new) {
-		XLALPrintError("memory allocation failure\n");
-		goto error;
-	}
-
-	/* unpack the base columns.  have to do the strings manually
-	 * because they get copied by value rather than reference. */
-	result_code = ligolw_table_unpack_row(table, *row, spec = sim_burst_basic);
-	if(result_code)
-		goto unpackerror;
-
-	/* do this after ligolw_table_unpack_row() to let it confirm the
-	 * column is present and has the correct type.  we've confirmed the
-	 * column exists and has the correct type, so shouldn't need to
-	 * check for errors */
-	str = ligolw_row_get_cell(row, "waveform", NULL);
-	strncpy(new->waveform, str->as_string, LIGOMETA_WAVEFORM_MAX - 1);
-	new->waveform[LIGOMETA_WAVEFORM_MAX-1] = '\0';
-	/* we are treated as taking ownership of this, so we need to free
-	 * it */
-	free(str->as_string);
-	str->as_string = NULL;
-
-	/* unpack additional columns depending on the waveform */
-	if(!strcmp(new->waveform, "StringCusp"))
-		result_code = ligolw_table_unpack_row(table, *row, spec = sim_burst_StringCusp);
-	else if(!strcmp(new->waveform, "SineGaussian") || !strcmp(new->waveform, "SineGaussianF"))
-		result_code = ligolw_table_unpack_row(table, *row, spec = sim_burst_SineGaussian);
-	else if(!strcmp(new->waveform, "Gaussian"))
-		result_code = ligolw_table_unpack_row(table, *row, spec = sim_burst_Gaussian);
-	else if(!strcmp(new->waveform, "BTLWNB"))
-		result_code = ligolw_table_unpack_row(table, *row, spec = sim_burst_BTLWNB);
-	else if(!strcmp(new->waveform, "Impulse"))
-		result_code = ligolw_table_unpack_row(table, *row, spec = sim_burst_Impulse);
-	else {
-		/* unrecognized waveform */
-		XLALPrintError("failure parsing row: unrecognized waveform \"%s\"\n", new->waveform);
-		goto error;
-	}
-	if(result_code)
-		goto unpackerror;
-
-	/* add new sim to head of linked list.  the linked list's elements
-	 * are reversed with respect to the file. */
-	new->next = *head;
-	*head = new;
-
-	/* success */
-	ligolw_table_free_row_data(table, row);
-	free(row);
-	return 0;
-
-unpackerror:
-	if(result_code > 0) {
-		/* missing required column */
-		XLALPrintError("failure parsing row: missing column \"%s\"\n", spec[result_code - 1].name);
-	} else if(result_code < 0) {
-		/* column type mismatch */
-		XLALPrintError("failure parsing row: incorrect type for column \"%s\"\n", spec[-result_code - 1].name);
-	}
-error:
-	free(new);
-	ligolw_table_free_row_data(table, row);
-	free(row);
-	return -1;
-}
 
 
 static int XLALTableFromLIGOLw(
